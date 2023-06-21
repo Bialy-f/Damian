1dcb2b0a90492cdf00447537a180a0dbf1b22f9c
Bialy-f <damian20-02@wp.pl>
2023-06-21 18:56:45 +0200

    Trzeci commit w którym zmieniono z kalkulatora na logowanie w pliku PHP.php

--- PHP.php
+++ PHP.php
@@ -1,14 +1,19 @@
 <?php
-$liczba1 = $_POST['liczba1'];
-$liczba2 = $_POST['liczba2'];
-echo 'Wynik działania '.$liczba1.' '.$_POST['znak'].' '.$liczba2.' wynosi ';
-switch($_POST['znak']) {
-  case "+":
-    echo $liczba1 + $liczba2; break;
-  case "-":
-    echo $liczba1 - $liczba2; break;
-  case "*":
-    echo $liczba1 * $liczba2; break;
-  case "/":
-    echo $liczba1 / $liczba2; break; }
-?> 
+ if(isset($_POST["submit"])){
+    if($_POST["haslo"] == "sekret1"){
+       ?>
+     Tutaj jest chroniona zawartość...
+      <?php
+    }
+    else{
+       echo "Nieprawidłowe hasło";
+    }
+ }
+ else{
+   echo "Wprowadź hasło";
+ }
+ ?>
+ <form method="POST">
+     <input type="password" name="haslo" placeholder="Podaj hasło">
+     <input type="submit" name="submit" value="Wyślij">
+ </form>
